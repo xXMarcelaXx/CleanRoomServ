b4640ddf12fb8b19f5f7138d458d40e6bafe4798
xXMarcelaXx <marcelacasesc@gmail.com>
2023-08-21 16:29:12 -0600

    error en impresora 3, en una hoja, metodo check

--- resources/views/hojaschequeo/hojas5-7/F7-SETCS-ELE-60-L1-06-4-2/check.blade.php
+++ resources/views/hojaschequeo/hojas5-7/F7-SETCS-ELE-60-L1-06-4-2/check.blade.php
@@ -41,7 +41,8 @@
     </div>
     <div class="table-responsive">
         <div class="table-responsive" align="center">
-
+            @foreach ($datoshojas as $datoshoja)
+            @endforeach
             <table class="table table-success table-bordered">
                 <thead>
                     <tr>
